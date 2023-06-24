4fd5861b11d77e24766d4209b79c95a94bd96490
Andrew Sonin <asonin@tunatech.io>
2023-06-24 03:39:13 +0300

    docs(wrappers): add field descriptions

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -2,15 +2,24 @@ use crate::{ReadApi, WriteApi};
 
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`ReadApi`] wrapper for constant references.
-pub struct ReadApiWrapper<'a, T>(pub &'a T);
+pub struct ReadApiWrapper<'a, T>(
+    /// Wrapped reference.
+    pub &'a T
+);
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`RwApi`](crate::RwApi) wrapper for mutable references.
-pub struct RwApiWrapper<'a, T>(pub &'a mut T);
+pub struct RwApiWrapper<'a, T>(
+    /// Wrapped reference.
+    pub &'a mut T
+);
 
 #[derive(Default, Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`RwApi`](crate::RwApi) owning wrapper for solitary objects.
-pub struct RwApiWrapperOwned<T>(pub T);
+pub struct RwApiWrapperOwned<T>(
+    /// Wrapped owned object.
+    pub T
+);
 
 impl<'a, T> ReadApi for ReadApiWrapper<'a, T>
 {
