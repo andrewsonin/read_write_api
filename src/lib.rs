517e5f0ca6d1b2ca785e62526297f74083f396c9
Andrew Sonin <asonin@tunatech.io>
2023-06-25 19:01:38 +0300

    docs: add more examples

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,125 @@ pub use wrappers::{ReadApiWrapper, RwApiWrapper, RwApiWrapperOwned};
 mod impls;
 mod wrappers;
 
-#[doc = include_str!("../README.md")]
+/// Provides an interface to unify single-threaded code and RwLocks-based code.
+///
+/// A nice side effect of this trait is manifested in the activation
+/// of the borrow-checker to check the absence of deadlocks introduced in the current scope.
+/// This is because `Self::write` can now only be called on a mutable,
+/// but not constant, reference.
+///
+/// # Example
+///
+/// ```rust
+/// use parking_lot::RwLock;
+/// use read_write_api::{RwApi, RwApiWrapper, RwApiWrapperOwned};
+///
+/// fn do_something(mut x: impl RwApi<Target=u64>) -> u64 {
+///     if *x.read() == 1 {
+///         *x.write() = 2;
+///         *x.read()
+///     } else {
+///         *x.read()
+///     }
+/// }
+///
+/// assert_eq!(do_something(RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(RwApiWrapperOwned(3)), 3);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(3)), 3);
+///
+/// assert_eq!(do_something(RwLock::new(1)), 2);
+/// assert_eq!(do_something(RwLock::new(3)), 3);
+/// assert_eq!(do_something(&RwLock::new(1)), 2);
+/// assert_eq!(do_something(&RwLock::new(3)), 3);
+/// assert_eq!(do_something(&mut RwLock::new(1)), 2);
+/// assert_eq!(do_something(&mut RwLock::new(3)), 3);
+///
+/// fn do_something_ref<'a>(mut x: impl RwApi<Target=&'a mut u64>) -> u64 {
+///     if **x.read() == 1 {
+///         **x.write() = 2;
+///         **x.read()
+///     } else {
+///         **x.read()
+///     }
+/// }
+///
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 3)), 3);
+///
+/// assert_eq!(do_something_ref(RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 3)), 3);
+/// ```
 pub trait RwApi: ReadApi + WriteApi {}
 
 /// Provides a constant part of the [`RwApi`] interface.
+///
+/// # Example
+///
+/// ```rust
+/// use std::ops::Deref;
+/// use parking_lot::RwLock;
+/// use read_write_api::{ReadApi, RwApi, RwApiWrapper, RwApiWrapperOwned};
+///
+/// fn do_something(mut x: impl RwApi<Target=u64>) -> u64 {
+///     if *x.read() == 1 {
+///         *x.write() = 2;
+///         read_x(&x)
+///     } else {
+///         read_x(&x)
+///     }
+/// }
+///
+/// fn read_x(x: &impl ReadApi<Target=u64>) -> u64 {
+///     *x.read()
+/// }
+///
+/// assert_eq!(do_something(RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(RwApiWrapperOwned(3)), 3);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(3)), 3);
+///
+/// assert_eq!(do_something(RwLock::new(1)), 2);
+/// assert_eq!(do_something(RwLock::new(3)), 3);
+/// assert_eq!(do_something(&RwLock::new(1)), 2);
+/// assert_eq!(do_something(&RwLock::new(3)), 3);
+/// assert_eq!(do_something(&mut RwLock::new(1)), 2);
+/// assert_eq!(do_something(&mut RwLock::new(3)), 3);
+///
+/// fn do_something_ref<'a>(mut x: impl RwApi<Target=&'a mut u64>) -> u64 {
+///     if **x.read() == 1 {
+///         **x.write() = 2;
+///         read_x_ref(&x)
+///     } else {
+///         read_x_ref(&x)
+///     }
+/// }
+///
+/// fn read_x_ref<T>(x: &impl ReadApi<Target=T>) -> u64
+///     where
+///         T: Deref<Target=u64>
+/// {
+///     **x.read()
+/// }
+///
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 3)), 3);
+///
+/// assert_eq!(do_something_ref(RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 3)), 3);
+/// ```
 pub trait ReadApi: GuardedTarget
 {
     /// [`Self::read`] return type.
@@ -20,6 +135,60 @@ pub trait ReadApi: GuardedTarget
 }
 
 /// Provides a mutable part of the [`RwApi`] interface.
+///
+/// # Example
+///
+/// ```rust
+/// use std::ops::Deref;
+/// use parking_lot::RwLock;
+/// use read_write_api::{RwApi, RwApiWrapper, RwApiWrapperOwned, WriteApi};
+///
+/// fn do_something(mut x: impl RwApi<Target=u64>) -> u64 {
+///     if *x.read() == 1 {
+///         *write_x(&mut x) = 2;
+///         *x.read()
+///     } else {
+///         *x.read()
+///     }
+/// }
+///
+/// fn write_x<T: WriteApi>(x: &mut T) -> T::WriteGuard<'_> {
+///     x.write()
+/// }
+///
+/// assert_eq!(do_something(RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(RwApiWrapperOwned(3)), 3);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(1)), 2);
+/// assert_eq!(do_something(&mut RwApiWrapperOwned(3)), 3);
+///
+/// assert_eq!(do_something(RwLock::new(1)), 2);
+/// assert_eq!(do_something(RwLock::new(3)), 3);
+/// assert_eq!(do_something(&RwLock::new(1)), 2);
+/// assert_eq!(do_something(&RwLock::new(3)), 3);
+/// assert_eq!(do_something(&mut RwLock::new(1)), 2);
+/// assert_eq!(do_something(&mut RwLock::new(3)), 3);
+///
+/// fn do_something_ref<'a>(mut x: impl RwApi<Target=&'a mut u64>) -> u64 {
+///     if **x.read() == 1 {
+///         **write_x(&mut x) = 2;
+///         **x.read()
+///     } else {
+///         **x.read()
+///     }
+/// }
+///
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwApiWrapper(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwApiWrapper(&mut 3)), 3);
+///
+/// assert_eq!(do_something_ref(RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&RwLock::new(&mut 3)), 3);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 1)), 2);
+/// assert_eq!(do_something_ref(&mut RwLock::new(&mut 3)), 3);
+/// ```
 pub trait WriteApi: GuardedTarget
 {
     /// [`Self::write`] return type.
@@ -31,6 +200,42 @@ pub trait WriteApi: GuardedTarget
 }
 
 /// Provides a single dereferencing target type for [`ReadApi`], [`WriteApi`] and [`RwApi`].
+///
+/// # Example
+///
+/// ```rust
+/// use read_write_api::{GuardedTarget, ReadApi, RwApi, WriteApi};
+///
+/// struct A(u64);
+///
+/// impl GuardedTarget for A {
+///     type Target = u64;
+/// }
+///
+/// impl ReadApi for A
+/// {
+///     type ReadGuard<'a>  = &'a u64
+///         where Self: 'a;
+///
+///     fn read(&self) -> Self::ReadGuard<'_> {
+///         &self.0
+///     }
+/// }
+///
+/// impl WriteApi for A
+/// {
+///     type WriteGuard<'a> = &'a mut u64
+///         where Self: 'a;
+///
+///     fn write(&mut self) -> Self::WriteGuard<'_> {
+///         &mut self.0
+///     }
+/// }
+///
+/// fn accept_read_write(_: impl RwApi<Target=u64>) {}
+///
+/// accept_read_write(A(1))
+/// ```
 pub trait GuardedTarget
 {
     /// Dereference target of the read and write guards.
--- src/wrappers.rs
+++ src/wrappers.rs
@@ -1,5 +1,19 @@
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`ReadApi`](crate::ReadApi) wrapper for constant references.
+///
+/// # Example
+///
+/// ```rust
+/// use read_write_api::{ReadApi, ReadApiWrapper};
+/// use parking_lot::RwLock;
+///
+/// fn read<T: ReadApi>(x: &T) -> T::ReadGuard<'_> {
+///     x.read()
+/// }
+///
+/// let _ = read(&ReadApiWrapper(&1));
+/// let _ = read(&RwLock::new(2));
+/// ```
 pub struct ReadApiWrapper<'a, T>(
     /// Wrapped reference.
     pub &'a T
@@ -7,6 +21,10 @@ pub struct ReadApiWrapper<'a, T>(
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`RwApi`](crate::RwApi) wrapper for mutable references.
+///
+/// # Example
+///
+/// See the [`RwApi`](crate::RwApi) docs for usage examples.
 pub struct RwApiWrapper<'a, T>(
     /// Wrapped reference.
     pub &'a mut T
@@ -14,6 +32,10 @@ pub struct RwApiWrapper<'a, T>(
 
 #[derive(Default, Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 /// [`RwApi`](crate::RwApi) owning wrapper for solitary objects.
+///
+/// # Example
+///
+/// See the [`RwApi`](crate::RwApi) docs for usage examples.
 pub struct RwApiWrapperOwned<T>(
     /// Wrapped owned object.
     pub T
